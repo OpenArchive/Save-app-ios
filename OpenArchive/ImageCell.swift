72fd48ad810d94db3947423131e7d2a6fae4b58f
Benjamin Erhart <be@benjaminerhart.com>
2018-06-28 12:49:34 +0200

    Let user fetch image from gallery, show selected images in table view.

---
+++ OpenArchive/ImageCell.swift
@@ -0,0 +1,26 @@
+//
+//  ImageCell.swift
+//  OpenArchive
+//
+//  Created by Benjamin Erhart on 28.06.18.
+//  Copyright © 2018 Open Archive. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+
+    @IBOutlet var img: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- OpenArchive/MainViewController.swift
+++ OpenArchive/MainViewController.swift
@@ -7,8 +7,21 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class MainViewController: UITableViewController {
+class MainViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    lazy var imagePicker: UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = [kUTTypeImage as String]
+        imagePicker.modalPresentationStyle = .popover
+
+        return imagePicker
+    }()
+
+    lazy var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +33,41 @@ class MainViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: actions
+
     @IBAction func add(_ sender: UIBarButtonItem) {
-        print("Add pressed!")
+        imagePicker.popoverPresentationController?.barButtonItem = sender
+        present(imagePicker, animated: true)
     }
 
+    // MARK: UITableViewDataSource
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        
+        cell.img.image = images[indexPath.row]
+
+        return cell
+    }
+
+    // MARK: UIImagePickerControllerDelegate
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage ??
+            info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            images.append(image)
+
+            dismiss(animated: true, completion: nil)
+
+            tableView.reloadData()
+            tableView.scrollToRow(at: IndexPath(row: images.count - 1, section: 0),
+                                  at: UITableViewScrollPosition.bottom, animated: true)
+        }
+    }
 }
 
