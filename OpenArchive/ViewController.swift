8e3d31204309b1e70699d12688c9da9480bbb674
Benjamin Erhart <be@benjaminerhart.com>
2018-06-28 09:53:18 +0200

    Initial commit.

---
+++ OpenArchive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenArchive
+//
+//  Created by Benjamin Erhart on 28.06.18.
+//  Copyright © 2018 Open Archive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
