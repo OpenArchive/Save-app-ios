34ca62d6c43145d3eb4bb0aaaac3b100d96f66a0
Benjamin Erhart <berhart@netzarchitekten.com>
2019-02-08 12:11:33 +0100

    Added `created` timestamp as first element to group key to enable sorting by creation.

--- Shared/Models/AssetsByCollectionView.swift
+++ Shared/Models/AssetsByCollectionView.swift
@@ -9,9 +9,18 @@
 import UIKit
 import YapDatabase
 
+/**
+ A view which sections `Asset`s by the `Collection`s they belong to.
+
+ The group key used is sortable by
+ `created` date of the `Collection` and filterable by the `projectId` the
+ `Collection` belongs to.
+ */
 class AssetsByCollectionView: YapDatabaseAutoView {
 
     static let name = "assets_by_collection"
+
+    private static let separator: Character = "/"
 
     override init() {
         let grouping = YapDatabaseViewGrouping.withObjectBlock() {
@@ -29,25 +38,63 @@ class AssetsByCollectionView: YapDatabaseAutoView {
         super.init(grouping: grouping, sorting: sorting, versionTag: nil, options: nil)
     }
 
+    /**
+     - parameter asset: The `Asset` to generate a group key for.
+     - returns: a group key from the asset's collection which is sortable by
+        creation date, filterable by project ID and groupable by collection ID.
+        Will return `nil` if `Asset` is `nil`.
+    */
     class func groupKey(for asset: Asset?) -> String? {
         if let asset = asset {
-            return "\(asset.collection.projectId)/\(asset.collectionId)"
+            let collection = asset.collection
+            let created = String(Int(collection.created.timeIntervalSince1970))
+
+            return "\(created)\(separator)\(collection.projectId)\(separator)\(collection.id)"
         }
 
         return nil
     }
 
+    /**
+     - parameter group: A valid group key of this view.
+     - returns: the `created` part of the group key as a `Date`.
+        Will return `nil` if `group` is `nil` or the group string is invalid.
+    */
+    class func created(from group: String?) -> Date? {
+        if let group = group?.split(separator: separator),
+            let epoch = TimeInterval(String(group[0])) {
+
+            return Date(timeIntervalSince1970: epoch)
+        }
+
+        return nil
+    }
+
+    /**
+     - parameter group: A valid group key of this view.
+     - returns: the `projectid` part of the group key as a `String`.
+        Will return `nil` if `group` is `nil` or the group string is invalid.
+    */
     class func projectId(from group: String?) -> String? {
-        if let group = group {
-            return String(group.split(separator: "/")[0])
+        if let group = group?.split(separator: separator),
+            group.count > 1 {
+
+            return String(group[1])
         }
 
         return nil
     }
 
+    /**
+     - parameter group: A valid group key of this view.
+     - returns: the `collectionId` part of the group key as a `String`.
+        Will return `nil` if `group` is `nil` or the group string is invalid.
+     */
     class func collectionId(from group: String?) -> String? {
-        if let group = group {
-            return String(group.split(separator: "/")[1])
+        if let group = group?.split(separator: separator),
+            group.count > 2 {
+
+            return String(group[2])
         }
 
         return nil
