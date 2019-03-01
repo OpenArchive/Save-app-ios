c6e62bd70afe35200d52c19f414d84440477e346
Benjamin Erhart <berhart@netzarchitekten.com>
2019-03-01 13:23:13 +0100

    Towards issue #36: Add blur to non-uploaded images.

--- OpenArchive/Main/ImageCell.swift
+++ OpenArchive/Main/ImageCell.swift
@@ -11,12 +11,28 @@ import UIKit
 class ImageCell: UICollectionViewCell {
 
     static let reuseId = "imageCell"
+
+    private var blurView: UIVisualEffectView?
     
     @IBOutlet var imgView: UIImageView!
 
     var asset: Asset? {
         didSet {
             self.imgView.image = asset?.getThumbnail()
+
+            if !(asset?.isUploaded ?? false) && !UIAccessibility.isReduceTransparencyEnabled {
+                if blurView == nil {
+                    blurView = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight))
+                    blurView?.alpha = 0.35
+                    blurView?.frame = imgView.bounds
+                    blurView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                }
+
+                imgView.addSubview(blurView!)
+            }
+            else {
+                blurView?.removeFromSuperview()
+            }
         }
     }
 }
