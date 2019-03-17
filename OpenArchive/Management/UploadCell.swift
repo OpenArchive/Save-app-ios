558a65e3058c3b3d9d4d37e7d4a1cc133d16b8a2
Benjamin Erhart <berhart@netzarchitekten.com>
2019-03-17 14:32:23 +0100

    Added progress button tap handling.

--- OpenArchive/Management/UploadCell.swift
+++ OpenArchive/Management/UploadCell.swift
@@ -9,35 +9,37 @@
 import UIKit
 import DownloadButton
 
+protocol UploadCellDelegate {
+    func progressTapped(_ upload: Upload, _ button: PKDownloadButton)
+}
+
 class UploadCell: BaseCell, PKDownloadButtonDelegate {
 
     override class var reuseId: String {
-        return "assetCell"
+        return "uploadCell"
     }
 
-    @IBOutlet weak var progress: PKDownloadButton! {
-        didSet {
-            progress.delegate = self
-        }
-    }
+    @IBOutlet weak var progress: PKDownloadButton!
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var nameLb: UILabel!
 
-    var upload: Upload? {
+    weak var upload: Upload? {
         didSet {
-            progress.state = upload?.paused ?? true
-                ? .pending
-                : upload?.isUploaded ?? false ? .downloaded : .downloading
+            progress.state = upload?.state ?? .pending
             progress.stopDownloadButton.progress = CGFloat(upload?.progress ?? 0)
             thumbnail.image = upload?.thumbnail
             nameLb.text = upload?.filename
         }
     }
 
+    var delegate: UploadCellDelegate?
+
 
     // MARK: PKDownloadButtonDelegate
 
     func downloadButtonTapped(_ downloadButton: PKDownloadButton!, currentState state: PKDownloadButtonState) {
-        print("[\(String(describing: type(of: self)))]#downloadButtonTapped state=\(state)")
+        if let upload = upload {
+            delegate?.progressTapped(upload, downloadButton)
+        }
     }
 }
