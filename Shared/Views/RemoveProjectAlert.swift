3cea73baec02bb9c049c8f405adbc3989657b671
Benjamin Erhart <berhart@netzarchitekten.com>
2023-11-28 14:45:40 +0100

    Issue #243: Reworked wording of delete folder dialog, to get closer to design spec. (Which is has misleading text.)

--- Shared/Views/RemoveProjectAlert.swift
+++ Shared/Views/RemoveProjectAlert.swift
@@ -11,8 +11,10 @@ import UIKit
 class RemoveProjectAlert: UIAlertController {
 
     convenience init(_ project: Project, _ completionHandler: ((_ success: Bool) -> Void)? = nil) {
-        self.init(title: NSLocalizedString("Remove from App", comment: ""),
-                   message: String(format: NSLocalizedString("Are you sure you want to remove your project \"%@\"?", comment: ""), project.name ?? ""),
+        self.init(title: NSLocalizedString("Are you sure?", comment: ""),
+                  message: String(format: NSLocalizedString(
+                    "Removing this folder will remove all contained thumbnails from the %@ app.",
+                    comment: "Placeholder is app name"), Bundle.main.displayName),
                    preferredStyle: .alert)
 
         addAction(AlertHelper.cancelAction(handler: { _ in
@@ -23,7 +25,7 @@ class RemoveProjectAlert: UIAlertController {
             }
         }))
 
-        addAction(AlertHelper.destructiveAction(NSLocalizedString("Remove", comment: ""), handler: { _ in
+        addAction(AlertHelper.destructiveAction(NSLocalizedString("Remove Folder", comment: ""), handler: { _ in
             Db.writeConn?.asyncReadWrite() { tx in
                 tx.remove(project)
 
