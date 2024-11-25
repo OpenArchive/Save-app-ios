80d4e7b494d33db1df25f974813fe0da1572aeeb
Navoda Sathsarani <navoda.sathsarani@axiatadigitallabs.com>
2024-11-25 19:13:39 +0530

    scrollview added

--- Save/Features/InternetArchive/Presentation/Login/InternetArchiveLoginView.swift
+++ Save/Features/InternetArchive/Presentation/Login/InternetArchiveLoginView.swift
@@ -30,108 +30,121 @@ struct InternetArchiveLoginContent: View {
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        
-        VStack(alignment: .center) {
-            HStack {
-                Circle().fill(colorScheme == .dark ? Color.white : Color.pillBackground)
-                    .frame(width: 80, height: 80)
-                    .overlay(
-                        Image("InternetArchiveLogo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 48, height: 48)
-                    ).padding(.trailing, 6)
-                VStack(alignment: .leading) {
-                    Text(LocalizedStringKey("Internet Archive")).font(.headline)
-                    Text(LocalizedStringKey("Upload your media to a free public or paid private account on the Internet Archive.")).font(.subheadline)
-                    
-                }
-            }.padding()
-            
-            Spacer()
-            
-            TextField(LocalizedStringKey("Username"), text: state.userName)
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
-                .padding(.leading)
-                .padding(.trailing)
-            
-            
-            HStack {
-                if (isShowPassword) {
-                    TextField(LocalizedStringKey("Password"), text: state.password)
-                } else {
-                    SecureField(LocalizedStringKey("Password"), text: state.password)
-                }
-                Button(action: { isShowPassword = !isShowPassword}) {
-                    Image(systemName: isShowPassword ? "eye.slash" : "eye").foregroundColor(.gray)
-                }
-            }.padding().overlay(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color.gray, lineWidth: 1)
-            )
-            .padding(.leading)
-            .padding(.trailing)
-            
-            Spacer()
-            
-            if (state.isLoginError) {
-                Text(LocalizedStringKey("Invalid username or password")).foregroundColor(.red).padding()
-            }
-            
-            HStack(alignment: .center) {
-                Text(LocalizedStringKey("No Account?"))
-                Button(action: {
-                    dispatch(.CreateAccount)
-                }) {
-                    Text(LocalizedStringKey("Create one"))
-                }.foregroundColor(.accent)
-            }
-            
-            Spacer()
-            
-            HStack(alignment: .bottom) {
-                Button(action: {
-                    dispatch(.Cancel)
-                }, label: {
-                    Text(LocalizedStringKey("Cancel"))
-                }).padding().frame(maxWidth: .infinity).foregroundColor(.accent)
-                
-                Button(action: {
-                    dispatch(.Login)
-                }, label: {
-                    
-                    if (state.isBusy) {
-                        ActivityIndicator(style: .medium, animate: .constant(true)).foregroundColor(.black)
-                    } else {
-                        Text(LocalizedStringKey("Login"))
+        GeometryReader { reader in
+            ScrollView {
+                VStack {
+                    HStack {
+                        Circle().fill(colorScheme == .dark ? Color.white : Color.pillBackground)
+                            .frame(width: 80, height: 80)
+                            .overlay(
+                                Image("InternetArchiveLogo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 48, height: 48)
+                            ).padding(.trailing, 6)
+                        VStack(alignment: .leading) {
+                            Text(LocalizedStringKey("Internet Archive")).font(.headline)
+                            Text(LocalizedStringKey("Upload your media to a free public or paid private account on the Internet Archive.")).font(.subheadline)
+                        }
                     }
-                })
-                .disabled(!state.isValid)
-                .padding()
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                .background(Color.accent)
-                .foregroundColor(.black)
-                .cornerRadius(12)
-            }.padding()
-        }.padding(.bottom, keyboardOffset)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .onAppear(perform: setupKeyboardObservers)
-            .onDisappear(perform: removeKeyboardObservers)
-            .animation(.easeOut(duration: 0.3))
+                    .padding()
+                  
+                    Spacer()
+                           
+                           TextField(LocalizedStringKey("Username"), text: state.userName)
+                               .autocapitalization(.none)
+                               .padding()
+                               .overlay(
+                                   RoundedRectangle(cornerRadius: 8)
+                                       .stroke(Color.gray, lineWidth: 1)
+                               )
+                               .padding(.leading)
+                               .padding(.trailing)
+                           
+                           
+                           HStack {
+                               if (isShowPassword) {
+                                   TextField(LocalizedStringKey("Password"), text: state.password)
+                               } else {
+                                   SecureField(LocalizedStringKey("Password"), text: state.password)
+                               }
+                               Button(action: { isShowPassword = !isShowPassword}) {
+                                   Image(systemName: isShowPassword ? "eye.slash" : "eye").foregroundColor(.gray)
+                               }
+                           }.padding().overlay(
+                               RoundedRectangle(cornerRadius: 8)
+                                   .stroke(Color.gray, lineWidth: 1)
+                           )
+                           .padding(.leading)
+                           .padding(.trailing)
+                           
+                           Spacer()
+                           
+                           if (state.isLoginError) {
+                               Text(LocalizedStringKey("Invalid username or password")).foregroundColor(.red).padding()
+                           }
+                           
+                           HStack(alignment: .center) {
+                               Text(LocalizedStringKey("No Account?"))
+                               Button(action: {
+                                   dispatch(.CreateAccount)
+                               }) {
+                                   Text(LocalizedStringKey("Create one"))
+                               }.foregroundColor(.accent)
+                           }
+                           
+                           Spacer()
+                    Spacer()  // This spacer will push the buttons to the bottom
+                    
+                    HStack(alignment: .bottom) {
+                        Button(action: {
+                            dispatch(.Cancel)
+                        }, label: {
+                            Text(LocalizedStringKey("Cancel"))
+                        })
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.accent)
+                        
+                        Button(action: {
+                            dispatch(.Login)
+                        }, label: {
+                            if (state.isBusy) {
+                                ActivityIndicator(style: .medium, animate: .constant(true)).foregroundColor(.black)
+                            } else {
+                                Text(LocalizedStringKey("Login"))
+                            }
+                        })
+                        .disabled(!state.isValid)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accent)
+                        .foregroundColor(.black)
+                        .cornerRadius(12)
+                    }
+                    .padding()
+                }
+                .frame(minHeight: reader.size.height)
+            }.padding(.bottom, keyboardOffset)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .onAppear(perform: setupKeyboardObservers)
+                .onDisappear(perform: removeKeyboardObservers)
+                .animation(.easeOut(duration: 0.3))
+        }
+       
+        
     }
+
+    
     private func setupKeyboardObservers() {
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
             if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-                self.keyboardOffset = keyboardFrame.height + GeneralConstants.constraint_30
+                self.keyboardOffset = keyboardFrame.height
             }
         }
         
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
-            self.keyboardOffset = GeneralConstants.constraint_zero
+            self.keyboardOffset = 0
         }
     }
     
@@ -140,6 +153,9 @@ struct InternetArchiveLoginContent: View {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
+
+
+
 
 struct InternetArchiveLoginView_Previews: PreviewProvider {
     static let state = InternetArchiveLoginState(
