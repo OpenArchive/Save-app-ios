d76ec70db61ba77658b0392306c50cc38b93b6de
Navoda Sathsarani <navoda.sathsarani_adl@myboostbank.co>
2025-11-22 01:56:29 +0530

    added placeholders for files. corrected localizations

--- Save/Main/TableCells/ImageCell.swift
+++ Save/Main/TableCells/ImageCell.swift
@@ -9,24 +9,28 @@
 import UIKit
 
 class ImageCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var fileName: UILabel!
+    @IBOutlet weak var fileImage: UIImageView!
+    @IBOutlet weak var defaultFileType: UIView!
+    
     static let reuseId = "imageCell"
-
+    
     private var blurViewDark: UIVisualEffectView?
     private var blurViewLight: UIVisualEffectView?
-
+    
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var progress: ProgressButton!
     @IBOutlet weak var errorIcon: UIImageView!
     @IBOutlet weak var movieIndicator: MovieIndicator!
-
+    
     private lazy var selectedView = SelectedView()
-
+    
     var highlightNonUploaded = true
-
+    
     private(set) weak var asset: Asset?
     private(set) weak var upload: Upload?
-
+    
     override var isSelected: Bool {
         didSet {
             if isSelected {
@@ -37,60 +41,97 @@ class ImageCell: UICollectionViewCell {
             }
         }
     }
-
+    
     func set(_ asset: Asset?, _ upload: Upload?) {
+        fileName.text = asset?.filename
         self.asset = asset
         self.upload = upload
-
-        imgView.image = asset?.getThumbnail()
-
-        if highlightNonUploaded && !(asset?.isUploaded ?? false) && !UIAccessibility.isReduceTransparencyEnabled {
-            if upload?.state == .uploading || upload?.state == .pending && upload?.error == nil {
-                if blurViewDark == nil {
-                    blurViewDark = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-                    blurViewDark?.alpha = 0.65
-                    blurViewDark?.frame = imgView.bounds
-                    blurViewDark?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+       
+        if asset?.hasThumbnail() == true {
+            imgView.isHidden = false
+            defaultFileType.isHidden = true
+            imgView.isHidden = false
+            
+            // Get thumbnail asynchronously
+            asset?.getThumbnailAsync { [weak self] image in
+                guard let self = self else { return }
+                
+                self.imgView.image = image
+                
+                applyBlurIfNeeded(to: imgView, asset: asset, upload: upload)
+                if asset?.isAv ?? false {
+                    movieIndicator.isHidden = false
+                    movieIndicator.set(duration: asset?.duration)
                 }
-
-                imgView.addSubview(blurViewDark!)
-            }
-            else {
-                if blurViewLight == nil {
-                    blurViewLight = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight))
-                    blurViewLight?.alpha = 0.35
-                    blurViewLight?.frame = imgView.bounds
-                    blurViewLight?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                else {
+                    movieIndicator.isHidden = true
                 }
-
-                imgView.addSubview(blurViewLight!)
             }
-        }
-        else {
-            blurViewDark?.removeFromSuperview()
-            blurViewLight?.removeFromSuperview()
-        }
-
-        if asset?.isAv ?? false {
-            movieIndicator.isHidden = false
-            movieIndicator.set(duration: asset?.duration)
-        }
-        else {
+        } else {
+            
+            imgView.isHidden = true
+            defaultFileType.isHidden = false
             movieIndicator.isHidden = true
+            fileImage.image = UIImage(named: asset?.getFileType().placeholder ?? "unknown")
+            
+            applyBlurIfNeeded(to: defaultFileType, asset: asset, upload: upload)
+            
         }
-
-        if upload?.error != nil {
-            errorIcon.isHidden = false
-            progress.isHidden = true
-
-            return
-        }
-
-        errorIcon.isHidden = true
-
+        
         progress.isHidden = upload == nil || asset?.isUploaded ?? true || upload?.state == .uploaded
         progress.state = upload?.state ?? .pending
         print("upload progress\(String(describing: upload?.progress))")
         progress.progress = upload?.progress ?? 0
+        
+        errorIcon.isHidden = true
+        
+        if upload?.error != nil {
+            errorIcon.isHidden = false
+            progress.isHidden = true
+            
+            return
+        }
+        
+    }
+    
+    private func applyBlurIfNeeded(to superview: UIView, asset: Asset?, upload: Upload?) {
+       
+        guard highlightNonUploaded,
+              !(asset?.isUploaded ?? false),
+              !UIAccessibility.isReduceTransparencyEnabled else {
+          
+            blurViewDark?.removeFromSuperview()
+            blurViewLight?.removeFromSuperview()
+            return
+        }
+      
+        let shouldUseDarkBlur = (upload?.state == .uploading || upload?.state == .pending) && upload?.error == nil
+        
+        if shouldUseDarkBlur {
+           
+            blurViewLight?.removeFromSuperview()
+       
+            if blurViewDark == nil {
+                blurViewDark = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+                blurViewDark?.alpha = 0.65
+                blurViewDark?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            }
+            
+            blurViewDark?.frame = superview.bounds
+            superview.addSubview(blurViewDark!)
+        } else {
+          
+            blurViewDark?.removeFromSuperview()
+            
+            if blurViewLight == nil {
+                blurViewLight = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight))
+                blurViewLight?.alpha = 0.35
+                blurViewLight?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            }
+            
+            blurViewLight?.frame = superview.bounds
+            superview.addSubview(blurViewLight!)
+        }
     }
 }
