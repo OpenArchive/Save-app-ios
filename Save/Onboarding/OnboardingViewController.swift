af899c723a7d6fd14bce004c083527a57152b161
Navoda Sathsarani <navoda.sathsarani_adl@myboostbank.co>
2026-03-11 02:21:05 +0530

    slideview controller migration

---
+++ Save/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  OnboardingViewController.swift
+//  Save
+//
+//  Copyright © 2025 Open Archive. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+final class OnboardingViewController: UIHostingController<OnboardingView> {
+
+    required init() {
+        super.init(rootView: OnboardingView(onComplete: { _ in }))
+    }
+
+    @MainActor required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rootView = OnboardingView(onComplete: { [weak self] enableTor in
+            self?.complete(enableTor: enableTor)
+        })
+    }
+
+    private func complete(enableTor: Bool) {
+        if enableTor {
+            Settings.useTor = true
+        }
+        Settings.firstRunDone = true
+
+        if let navC = navigationController as? MainNavigationController {
+            navC.setRoot()
+        }
+    }
+}
